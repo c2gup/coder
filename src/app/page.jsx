4c611dded8df251c1d771fd6a9be6b1133b49075
Anupam gupta <c2gupt@gmail.com>
2026-02-14 01:14:06 +0530

    O1-project setup

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <p className="text-lg">This is a simple Next.js application.</p>
+     
+    </div>
+  );
+}
